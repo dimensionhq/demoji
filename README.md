0963000f31d3a4db14e66d555af991b17304e417
XtremeDevX <xtremedevx@gmail.com>
2021-11-09 15:27:44 +0400

    feat: `0.0.1` release

---
+++ README.md
@@ -0,0 +1,25 @@
+# Demoji
+
+A lightweight and snappy crate to remove emojis from a string.
+
+## Overview
+
+This crate allows for removal of emojis given a `String`, and that's it!
+
+This crate aims to be:
+
+- Fast
+- Lightweight
+- Easy To Use
+
+## Examples
+
+```rust
+// Remove all emojis from this string
+let demojified_string = demoji(String::from("⚡hel✅🙂lo🙂"))
+// Output: `hello`
+```
+
+### What does the name mean?
+
+Well, you could interpret it as `demolish-emoji` or `de-emoji` 😁.
---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+///! A lightweight and snappy crate to remove emojis from a string.
+///!
+///! ## Overview
+///!
+///! This crate allows for removal of emojis given a `String`, and that's it!
+///!
+///! This crate aims to be:
+///!
+///! - Fast
+///! - Lightweight
+///! - Easy To Use
+///!
+///! ## Examples
+///!
+///! ```rust
+///! // Remove all emojis from this string
+///! let demojified_string = demoji(String::from("⚡hel✅🙂lo🙂"))
+///! // Output: `hello`
+///! ```
+use regex::Regex;
+
+/// Removes all emojis from a string **(retains chinese characters)**
+///
+/// # Arguments
+///
+/// * `string` - String with emojis
+///
+/// # Returns
+///
+/// * `String` - De-emojified string
+///
+/// # Examples
+///
+/// ```
+///
+/// // Remove all emojis from this string
+/// let demojified_string = demoji(String::from("⚡hel✅🙂lo🙂"))
+/// // Output: `hello`
+/// ```
+pub fn demoji(string: String) -> String {
+    let regex = Regex::new(concat!(
+        "[",
+        "\u{01F600}-\u{01F64F}", // emoticons
+        "\u{01F300}-\u{01F5FF}", // symbols & pictographs
+        "\u{01F680}-\u{01F6FF}", // transport & map symbols
+        "\u{01F1E0}-\u{01F1FF}", // flags (iOS)
+        "\u{002702}-\u{0027B0}",
+        "\u{0024C2}-\u{01F251}",
+        "]+",
+    ))
+    .unwrap();
+
+    regex.replace_all(&string, "").to_string()
+}
