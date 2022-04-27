af08daa4407fcb66ec45aaf62aa9d303521c7e27
y21 <btimo8822@gmail.com>
2022-04-27 10:09:23 +0200

    cache regex, fix test, reuse allocations

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,14 @@
 //! ## Examples
 //!
 //! ```rust
+//! # use demoji::demoji;
 //! // Remove all emojis from this string
 //! let demojified_string = demoji("⚡hel✅🙂lo🙂");
-//! assert_eq!(demojified_string, String::from("hello"));
+//! assert_eq!(demojified_string, "hello");
 //! ```
+use once_cell::sync::Lazy;
 use regex::Regex;
+use std::borrow::Cow;
 
 /// Removes all emojis from a string **(retains chinese characters)**
 ///
@@ -27,46 +30,51 @@ use regex::Regex;
 ///
 /// # Returns
 ///
-/// * `String` - De-emojified string
+/// * `Cow<str>` - De-emojified string
+///
+/// If no emojis are found in the given string, then the original string is returned, avoiding the allocation of a new string.
 ///
 /// # Examples
 ///
 /// ```
-///
+/// # use demoji::demoji;
 /// // Remove all emojis from this string
-/// let demojified_string = demoji("⚡hel✅🙂lo🙂")
+/// let demojified_string = demoji("⚡hel✅🙂lo🙂");
 /// assert_eq!(demojified_string, String::from("hello"));
 /// // Output: `hello`
 /// ```
-pub fn demoji(string: &str) -> String {
-    let regex = Regex::new(concat!(
-        "[",
-        "\u{01F600}-\u{01F64F}", // emoticons
-        "\u{01F300}-\u{01F5FF}", // symbols & pictographs
-        "\u{01F680}-\u{01F6FF}", // transport & map symbols
-        "\u{01F1E0}-\u{01F1FF}", // flags (iOS)
-        "\u{002702}-\u{0027B0}",
-        "\u{0024C2}-\u{01F251}",
-        "]+",
-    ))
-    .unwrap();
+pub fn demoji(string: &str) -> Cow<'_, str> {
+    // cache regex to avoid re-compiling it every call
+    static REGEX: Lazy<Regex> = Lazy::new(|| {
+        Regex::new(concat!(
+            "[",
+            "\u{01F600}-\u{01F64F}", // emoticons
+            "\u{01F300}-\u{01F5FF}", // symbols & pictographs
+            "\u{01F680}-\u{01F6FF}", // transport & map symbols
+            "\u{01F1E0}-\u{01F1FF}", // flags (iOS)
+            "\u{002702}-\u{0027B0}",
+            "\u{0024C2}-\u{01F251}",
+            "]+",
+        ))
+        .unwrap()
+    });
 
-    regex.replace_all(&string, "").to_string()
+    REGEX.replace_all(&string, "")
 }
 
 /// Demojify `String` and `&str`
 pub trait Demoji {
-    fn demojify(&self) -> String;
+    fn demojify(&self) -> Cow<'_, str>;
 }
 
 impl Demoji for &str {
-    fn demojify(&self) -> String {
+    fn demojify(&self) -> Cow<'_, str> {
         demoji(self)
     }
 }
 
 impl Demoji for String {
-    fn demojify(&self) -> String {
+    fn demojify(&self) -> Cow<'_, str> {
         demoji(&self)
     }
 }
